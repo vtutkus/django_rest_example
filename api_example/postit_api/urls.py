b831b9ddd36ad678bb4424b2934a0477e46e2aab
Vytautas <v.tutkus@gmail.com>
2022-03-30 20:49:49 +0300

    comment_api working

--- api_example/postit_api/urls.py
+++ api_example/postit_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import PostList, PostDetail, CommentList, CommentDetail, PostLikeCreate, UserCreate
+from .views import PostList, PostDetail, CommentList, CommentDetail, PostLikeCreate, UserCreate, CommentLikeCreate
 
 urlpatterns = [
     path('posts', PostList.as_view()),
     path('posts/<int:pk>', PostDetail.as_view()),
     path('posts/<int:pk>/comments', CommentList.as_view()),
     path('comments/<int:pk>', CommentDetail.as_view()),
+    path('comments/<int:pk>/like', CommentLikeCreate.as_view()),
     path('posts/<int:pk>/like', PostLikeCreate.as_view()),
     path('signup', UserCreate.as_view())
 ]
